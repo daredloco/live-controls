0218289cb5a077fa1a22d2ce7287a6356e79b96f
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-25 12:59:12 -0300

    Update config.php

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    //Admin Interface
+    'admininterface_enabled' => true, //Set this to true if you want to enable the admin interface
+    'admininterface_master' => 1, //Set this to the user id you want to give master access
+
+    //User groups
+    'usergroups_enabled' => true, //Set this to true if usergroups should be enabled
+    'usergroups_root' => 'root', //The key of the usergroup with root access
+
+    //User permissions
+    'userpermissions_enabled' => true, //Set this to true if userpermissions should be enabled
+];
