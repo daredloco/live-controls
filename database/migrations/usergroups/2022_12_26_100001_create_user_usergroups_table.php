b8e5f4e8deddee8a713dbd78a7ec1cc84ec481bc
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-26 10:42:35 -0300

    Fixed migrations

--- database/migrations/usergroups/2022_12_26_100001_create_user_usergroups_table.php
+++ database/migrations/usergroups/2022_12_26_100001_create_user_usergroups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserUserGroupsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
