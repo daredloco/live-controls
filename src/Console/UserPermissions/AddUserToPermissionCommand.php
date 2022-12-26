6009d99433920c9b1065dbbabd04384e0ab2de20
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-26 12:19:47 -0300

    Added groups to permission commands

--- src/Console/UserPermissions/AddUserToPermissionCommand.php
+++ src/Console/UserPermissions/AddUserToPermissionCommand.php
@@ -4,38 +4,73 @@ namespace Helvetiapps\LiveControls\Console\UserPermissions;
 
 use Illuminate\Console\Command;
 use App\Models\User;
+use Helvetiapps\LiveControls\Models\UserGroups\UserGroup;
 use Helvetiapps\LiveControls\Models\UserPermissions\UserPermission;
 
 class AddUserToPermissionCommand extends Command
 {
     protected $signature = 'livecontrols:setpermission';
 
-    protected $description = 'Adds an user to an userpermission';
+    protected $description = 'Adds an user/group to an userpermission';
 
     public function handle()
     {
-        $id = $this->ask('User ID');
-        $permissionkey = $this->ask('Permission Key');
+        $choice = $this->choice('User or Group?',[
+            'User',
+            'Group'
+        ]);
 
-        $user = User::find($id);
-        $permission = UserPermission::where('key', '=', $permissionkey)->first();
+        if($choice == 'User'){
+            //IS USER
+            $id = $this->ask('User ID');
+            $permissionkey = $this->ask('Permission Key');
+    
+            $user = User::find($id);
+            $permission = UserPermission::where('key', '=', $permissionkey)->first();
+    
+            if(is_null($permission)){
+                $this->warn('Invalid Permission Key!');
+                return;
+            }
+            if(is_null($user)){
+                $this->warn('Invalid User ID!');
+                return;
+            }
+    
+            if($permission->users()->where('users.id', '=', $id)->exists()){
+                $this->warn('User already has permission!');
+                return;
+            }
+    
+            $permission->users()->attach($id);
+    
+            $this->info('Added user to permission!');
+        }elseif($choice == 'Group'){
+            //IS GROUP
+            $key = $this->ask('Group Key');
+            $permissionkey = $this->ask('Permission Key');
 
-        if(is_null($permission)){
-            $this->warn('Invalid Permission Key!');
-            return;
+            $group = UserGroup::find($key);
+            $permission = UserPermission::where('key', '=', $permissionkey)->first();
+
+            if(is_null($permission)){
+                $this->warn('Invalid Permission Key!');
+                return;
+            }
+            if(is_null($group)){
+                $this->warn('Invalid Group Key!');
+                return;
+            }
+
+            if($permission->groups()->where('user_groups.key', '=', $key)->exists()){
+                $this->warn('Group already has permission!');
+                return;
+            }
+
+            $permission->groups()->attach($group->id);
+
+            $this->info('Added group to permission!');
         }
-        if(is_null($user)){
-            $this->warn('Invalid User ID!');
-            return;
-        }
-
-        if($permission->users()->where('users.id', '=', $id)->exists()){
-            $this->warn('User already has permission!');
-            return;
-        }
-
-        $permission->users()->attach($id);
-
-        $this->info('Added user to permission!');
+        
     }
 }
--- src/Console/UserPermissions/RemoveUserFromPermissionCommand.php
+++ src/Console/UserPermissions/RemoveUserFromPermissionCommand.php
@@ -5,36 +5,73 @@ namespace Helvetiapps\LiveControls\Console\UserPermissions;
 use Helvetiapps\LiveControls\Models\UserPermissions\UserPermission;
 use Illuminate\Console\Command;
 use App\Models\User;
+use Helvetiapps\LiveControls\Models\UserGroups\UserGroup;
 
 class RemoveUserFromPermissionCommand extends Command
 {
     protected $signature = 'livecontrols:unsetpermission';
 
-    protected $description = 'Removes an user from an userpermission';
+    protected $description = 'Removes an user/group from an userpermission';
 
     public function handle()
     {
-        $id = $this->ask('User ID');
-        $groupkey = $this->ask('Permission Key');
+        $choice = $this->choice('User or Group?',[
+            'User',
+            'Group'
+        ]);
 
-        $user = User::find($id);
-        $group = UserPermission::where('key', '=', $groupkey)->first();
+        if($choice == 'User'){
+            //IS USER
+            $id = $this->ask('User ID');
+            $permissionKey = $this->ask('Permission Key');
 
-        if(is_null($group)){
-            $this->warn('Invalid Permission Key!');
-            return;
-        }
-        if(is_null($user)){
-            $this->warn('Invalid User ID!');
-            return;
-        }
-        if(!$group->users()->where('users.id', '=', $id)->exists()){
-            $this->warn('User does not have this permission!');
-            return;
-        }
-        
-        $group->users()->detach($id);
+            $user = User::find($id);
+            if(is_null($user)){
+                $this->warn('Invalid User ID!');
+                return;
+            }
 
-        $this->info('Removed user from permission!');
+            $group = UserPermission::where('key', '=', $permissionKey)->first();
+
+            if(is_null($group)){
+                $this->warn('Invalid Permission Key!');
+                return;
+            }
+
+            if(!$group->users()->where('users.id', '=', $id)->exists()){
+                $this->warn('User does not have this permission!');
+                return;
+            }
+            
+            $group->users()->detach($id);
+
+            $this->info('Removed user from permission!');
+        }elseif($choice == 'Group'){
+            //IS GROUP
+            $key = $this->ask('Group Key');
+            $permissionKey = $this->ask('Permission Key');
+
+            $group = UserGroup::where('key', '=', $key)->first();
+            if(is_null($group)){
+                $this->warn('Invalid Group Key!');
+                return;
+            }
+
+            $permission = UserPermission::where('key', '=', $permissionKey)->first();
+
+            if(is_null($permission)){
+                $this->warn('Invalid Permission Key!');
+                return;
+            }
+
+            if(!$permission->groups()->where('user_groups.key', '=', $key)->exists()){
+                $this->warn('Group does not have this permission!');
+                return;
+            }
+            
+            $permission->groups()->detach($group->id);
+
+            $this->info('Removed group from permission!');
+        }
     }
 }
