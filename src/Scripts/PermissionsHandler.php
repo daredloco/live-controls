9ecbaf0c24669afa4ca91888ad2c1b36d83a1501
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-26 13:09:37 -0300

    Removed debug lines

--- src/Scripts/PermissionsHandler.php
+++ src/Scripts/PermissionsHandler.php
@@ -51,8 +51,6 @@ class PermissionsHandler{
 
         $groups = $user->groups;
 
-        dd(auth()->user(), $groups, $this->permissions);
-
         foreach($this->permissions as $permission){
             $perm = UserPermission::where('key', '=', $permission)->first();
             if(is_null($perm)){
