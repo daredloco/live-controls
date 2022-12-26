670477bc8d8b2883aac80b9eebf68bcaf0e2a247
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-26 08:41:26 -0300

    Another try to fix migrations

--- database/migrations/usergroups/2022_12_26_100001_create_user_usergroups_table.php
+++ database/migrations/usergroups/2022_12_26_100001_create_user_usergroups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUserUserGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_usergroup');
+        Schema::dropIfExists('user_usergroups');
     }
 };
