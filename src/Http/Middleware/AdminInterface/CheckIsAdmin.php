74e0e7a67f5554f733c1926c205e1c4a92cc5fd4
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-26 10:51:14 -0300

    Updated Admin Check with the new system

--- src/Http/Middleware/AdminInterface/CheckIsAdmin.php
+++ src/Http/Middleware/AdminInterface/CheckIsAdmin.php
@@ -15,11 +15,27 @@ class CheckIsAdmin
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, string $groupkey)
+    public function handle(Request $request, Closure $next, string $groupKey)
     {
-        if($groupkey != config('livecontrols.usergroups_admin') && auth()->id() != config('livecontrols.admininterface_master')){
-            abort(403);
+        //Pass if user is master
+        if(auth()->id() == config('livecontrols.admininterface_master')){
+            return $next($request);
         }
-        return $next($request);
+
+        //Pass if user is in admin group(s)
+        if(is_array(config('livecontrols.usergroups_admins'))){
+            foreach(config('livecontrols.usergroups_admins') as $adminKey){
+                if($adminKey == $groupKey){
+                    return $next($request);
+                }
+            }
+        }else{
+            if($groupKey == config('livecontrols.usergroups_admins')){
+                return $next($request);
+            }
+        }
+
+        //Throw 403 if user is neither master nor in an admin group
+        abort(403);
     }
 }
