5d7478c48b568d3a1f26793170adf9660d02f66a
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 14:25:16 -0300

    Adde permissions list

--- resources/views/livewire/admin/permission-list.blade.php
+++ resources/views/livewire/admin/permission-list.blade.php
@@ -1,3 +1,37 @@
 <div>
-    Dashboard!
+    <div class="col-md-4">
+        <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." wire:model='search'>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Key</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($permissions as $permission)
+                    <tr class="">
+                        <th scope="row">{{ $permission->id }}</th>
+                        <td>{{ $permission->name }}</td>
+                        <td>{{ $permission->key }}</td>
+                        <td>
+                            <a href="{{ route('livecontrols.admin.userpermissions.edit', ['userPermission' => $permission->id]) }}">Edit</a> 
+                            <a href="{{ route('livecontrols.admin.userpermissions.delete', ['userPermission' => $permission->id]) }}" onclick="event.preventDefault(); document.delete{{ $permission->id }}Form.submit();">Delete</a>
+                            <form name="delete{{$permission->id}}Form" action="{{ route('livecontrols.admin.userpermissions.delete', ['userPermission' => $permission->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                            </form>    
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <a href="{{ route('livecontrols.admin.userpermissions.create') }}" class="btn btn-success text-white">Create</a>
+
 </div>
--- src/Http/Livewire/Admin/PermissionList.php
+++ src/Http/Livewire/Admin/PermissionList.php
@@ -3,11 +3,20 @@
 namespace Helvetiapps\LiveControls\Http\Livewire\Admin;
 
 use Livewire\Component;
+use Helvetiapps\LiveControls\Models\UserPermissions\UserPermission;
 
 class PermissionList extends Component
 {
+    public $search = '';
+
     public function render()
     {
-        return view('livecontrols::livewire.admin.permission-list');
+        if($this->search != ''){
+            $permissions = UserPermission::where('name', 'LIKE', '%'.$this->search.'%')->paginate();
+        }else{
+            $permissions = UserPermission::paginate();
+        }
+
+        return view('livecontrols::livewire.admin.permission-list', ['permissions' => $permissions]);
     }
 }
