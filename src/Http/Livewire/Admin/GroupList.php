f4c9f85a7660ed363c4e74fbb9adc25a4bf3a154
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 15:06:33 -0300

    Update GroupList.php

--- src/Http/Livewire/Admin/GroupList.php
+++ src/Http/Livewire/Admin/GroupList.php
@@ -29,7 +29,7 @@ class GroupList extends Component
 
     public function editPermissions($id){
         $this->itemToEdit = UserGroup::find($id);
-        $this->itemPermissions = $this->itemToEdit->permissions;
+        $this->itemPermissions = $this->itemToEdit->permissions()->get()->toArray();
         $this->showPermissionModal = true;
     }
 
