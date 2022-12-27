1e6284dea197728ee3bb793a43b6094405307c69
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 15:35:38 -0300

    Fixed bug in HasPermissions trait

--- src/Traits/UserPermissions/HasPermissions.php
+++ src/Traits/UserPermissions/HasPermissions.php
@@ -2,12 +2,12 @@
 
 namespace Helvetiapps\LiveControls\Traits\UserPermissions;
 
-use Helvetiapps\LiveControls\Models\UserGroups\UserGroup;
+use Helvetiapps\LiveControls\Models\UserPermissions\UserPermission;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 trait HasPermissions{
     public function permissions(): BelongsToMany
     {
-        return $this->belongsToMany(UserGroup::class, 'user_userpermissions', 'user_permission_id', 'user_id');
+        return $this->belongsToMany(UserPermission::class, 'user_userpermissions', 'user_id', 'user_permission_id');
     }
 }
