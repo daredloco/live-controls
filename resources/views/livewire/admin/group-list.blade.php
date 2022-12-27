e92053c7cff95586de046e65caf9521d10371970
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 12:25:43 -0300

    Added Group List to Admin Interface

--- resources/views/livewire/admin/group-list.blade.php
+++ resources/views/livewire/admin/group-list.blade.php
@@ -1,3 +1,33 @@
 <div>
-    Dashboard!
+    <div class="col-md-4">
+        <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." wire:model='search'>
+    </div>
+
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Key</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($groups as $group)
+                    <tr class="">
+                        <th scope="row">{{ $group->id }}</th>
+                        <td>{{ $group->name }}</td>
+                        <td>{{ $group->key }}</td>
+                        <td>
+                            <a href="#">Edit</a> 
+                            <a href="#">Delete</a>    
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <a href="#" class="btn btn-success text-white">Create</a>
+
 </div>
--- src/Http/Livewire/Admin/GroupList.php
+++ src/Http/Livewire/Admin/GroupList.php
@@ -3,11 +3,20 @@
 namespace Helvetiapps\LiveControls\Http\Livewire\Admin;
 
 use Livewire\Component;
+use Helvetiapps\LiveControls\Models\UserGroups\UserGroup;
 
 class GroupList extends Component
 {
+    public $search = '';
+
     public function render()
     {
-        return view('livecontrols::livewire.admin.group-list');
+        if($this->search != ''){
+            $groups = UserGroup::where('name', 'LIKE', '%'.$this->search.'%')->paginate();
+        }else{
+            $groups = UserGroup::paginate();
+        }
+
+        return view('livecontrols::livewire.admin.user-list', ['groups' => $groups]);
     }
 }
