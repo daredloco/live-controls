9e569f2996a0b1be56025db19863e15abc644a3b
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 10:46:04 -0300

    Fixed admin dashboard view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
-Admin Dashboard
+    <x-slot name="header">
+        <h2 class="h4 font-weight-bold">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
 </x-app-layout>
