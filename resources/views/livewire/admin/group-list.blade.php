16b40d128da5b596d9fa8cdf7effdcdbb12f1d15
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 14:14:02 -0300

    Update group-list.blade.php

--- resources/views/livewire/admin/group-list.blade.php
+++ resources/views/livewire/admin/group-list.blade.php
@@ -17,7 +17,7 @@
                 @foreach($groups as $group)
                     <tr class="">
                         <th scope="row">{{ $group->id }}</th>
-                        <td>{{ $group->name }} <span  class="rounded-circle" style="background-color: {{ $group->color }};">&nbsp;</td>
+                        <td>{{ $group->name }} <span  class="rounded-circle" style="background-color: {{ $group->color }}; aspect-ratio: 1 / 1;">&nbsp;</td>
                         <td>{{ $group->key }}</td>
                         <td>
                             <a href="{{ route('livecontrols.admin.usergroups.edit', ['userGroup' => $group->id]) }}">Edit</a> 
