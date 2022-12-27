cac566ca2beb48c5e3ea44837ad12668be4de816
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-27 11:28:28 -0300

    Update main.blade.php

--- resources/views/livewire/admin/main.blade.php
+++ resources/views/livewire/admin/main.blade.php
@@ -38,6 +38,7 @@
             position: relative;
         }
         .livecontrols-sidebar a {float: left;}
+        }
 
         /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
         @media screen and (max-width: 400px) {
@@ -49,7 +50,7 @@
     </style>
     @endpush
     <div class="row">
-        <div class="col-md-4 livecontrols-sidebar">
+        <div class="col-md-3 livecontrols-sidebar">
             <a class="active" href="#dashboard" wire:click.prevent="changePage('dashboard')">Dashboard</a>
             <a href="#users" wire:click.prevent="changePage('users')">Users</a>
             <a href="#groups" wire:click.prevent="changePage('groups')">User Groups</a>
@@ -58,7 +59,7 @@
                 <a href="#{{ $key }}" wire:click.prevent="changePage('{{ $key }}')">{{ $label }}</a>
             @endforeach
         </div>
-        <div class="col-md-8 content">
+        <div class="col-md-9 content">
             @if($page == 'dashboard')
                 @livewire('livecontrols-admin-dashboard', [], key('admin-dashboard'))
             @elseif($page == 'users')
