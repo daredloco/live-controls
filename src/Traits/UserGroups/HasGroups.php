1026ad5f88f0afad6e1c55e60285f7e07948f923
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-28 07:48:58 -0300

    Fixed HasGroups trait

--- src/Traits/UserGroups/HasGroups.php
+++ src/Traits/UserGroups/HasGroups.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 trait HasGroups{
     public function groups(): BelongsToMany
     {
-        return $this->belongsToMany(UserGroup::class, 'user_usergroups', 'user_group_id', 'user_id');
+        return $this->belongsToMany(UserGroup::class, 'user_usergroups', 'user_id', 'user_group_id');
     }
 }
