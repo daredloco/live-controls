735e9919671cfc228e241df020217c5fd764d500
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-28 07:45:16 -0300

    Added missiong $groups variable to render function

--- src/Http/Livewire/Admin/UserList.php
+++ src/Http/Livewire/Admin/UserList.php
@@ -28,12 +28,13 @@ class UserList extends Component
         }
 
         $permissions = UserPermission::orderBy('name')->get();
+        $groups = UserGroup::orderBy('name')->get();
 
         $createRoute = config('livecontrols.routes_users')['create'] == '' ? false : config('livecontrols.routes_users')['create'];
         $editRoute = config('livecontrols.routes_users')['edit'] == '' ? false : config('livecontrols.routes_users')['edit'];
         $deleteRoute = config('livecontrols.routes_users')['delete'] == '' ? false : config('livecontrols.routes_users')['delete'];
 
-        return view('livecontrols::livewire.admin.user-list', ['users' => $users, 'permissions' => $permissions, 'createRoute' => $createRoute, 'editRoute' => $editRoute, 'deleteRoute' => $deleteRoute]);
+        return view('livecontrols::livewire.admin.user-list', ['users' => $users, 'permissions' => $permissions, 'groups' => $groups, 'createRoute' => $createRoute, 'editRoute' => $editRoute, 'deleteRoute' => $deleteRoute]);
     }
 
     public function editPermissions($id){
