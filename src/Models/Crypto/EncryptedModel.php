7e1aad3e436cc56078e0bced024a6c264cf64160
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-28 08:24:41 -0300

    Added EncryptedModel

---
+++ src/Models/Crypto/EncryptedModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Helvetiapps\LiveControls\Models\Crypto;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+
+class EncryptedModel extends Model
+{
+    public function create(array $attributes = [], array $ignoredFields = [], array $options = []){
+        $encAttributes = [];
+        foreach($attributes as $field => $attribute){
+            array_push($encAttributes, in_array($field, $ignoredFields) ? $attribute : Crypt::encrypt($attribute));
+        }
+        parent::__construct($encAttributes);
+        parent::save($options);
+    }
+
+    public function update(array $attributes = [], array $ignoredFields = [], array $options = []){
+        $encAttributes = [];
+        foreach($attributes as $field => $attribute){
+            array_push($encAttributes, in_array($field, $ignoredFields) ? $attribute : Crypt::encrypt($attribute));
+        }
+        parent::update($encAttributes, $options);
+    }
+}
