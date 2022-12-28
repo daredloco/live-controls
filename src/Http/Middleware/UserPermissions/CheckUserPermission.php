1c9de14acdfd1224ee0f3b303951e24fab9711a1
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-28 09:45:57 -0300

    Create CheckUserPermission.php

---
+++ src/Http/Middleware/UserPermissions/CheckUserPermission.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Helvetiapps\LiveControls\Http\Middleware\UserPermissions;
+
+use Closure;
+use Helvetiapps\LiveControls\Exceptions\InvalidUserPermissionException;
+use Helvetiapps\LiveControls\Models\UserPermissions\UserPermission;
+use Illuminate\Http\Request;
+
+class CheckUserPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, string ...$keys)
+    {
+        foreach($keys as $key){
+            $permission = UserPermission::where('key', '=', $key)->first();
+            if(is_null($permission)){
+                throw new InvalidUserPermissionException($key);
+            }
+            
+            if($permission->users()->where('user_id', '=', auth()->id())->exists()){
+                return $next($request);
+            }
+        }
+
+        abort(403);
+    }
+}
