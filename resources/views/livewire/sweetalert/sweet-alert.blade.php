82679e12d20f4239df6475a4b999c2159ca6fd18
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-29 14:47:01 -0300

    Update sweet-alert.blade.php

--- resources/views/livewire/sweetalert/sweet-alert.blade.php
+++ resources/views/livewire/sweetalert/sweet-alert.blade.php
@@ -37,7 +37,7 @@
                         console.log(results.size);
                     });
                     console.log(results.size);
-                    Livewire.emit(popupArr["confirmEvent"], Object.fromEntries(results););
+                    Livewire.emit(popupArr["confirmEvent"], Object.fromEntries(results));
                 }else if (result.isDenied){
                     Livewire.emit(popupArr["denyEvent"]);
                 }else if(result.isDismissed){
