8e844a6548c3dcfc9c7d3aaf668e6eb0cc27edc0
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-29 11:25:04 -0300

    Added min, max and step to NumericInput

--- src/Objects/SweetAlert/Inputs/NumericInput.php
+++ src/Objects/SweetAlert/Inputs/NumericInput.php
@@ -5,8 +5,38 @@ namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 class NumericInput extends Input {
     public string $inputType = 'number';
 
-    public function __construct($inputName, $label = "")
+    public ?int $minValue = null;
+    public ?int $maxValue = null;
+    public ?float $step = null;
+
+    public function __construct($inputName, $label = "", ?int $min = null, ?int $max = null, ?float $step = null)
     {
+        $this->minValue = $min;
+        $this->maxValue = $max;
+        $this->step = $step;
         parent::__construct($inputName, $label);
+    }
+
+    public function toArray():array{
+        $html = '<div class="'.$this->parentClass.'">';
+        if($this->label != null && $this->label != ''){
+            $html.= '<label for="'.$this->inputName.'" class="form-label">'.$this->label.'</label>';
+        }
+        $html .= '<input type="'.$this->inputType.'" class="'.$this->class.'" name="'.$this->inputName.'" id="'.$this->inputName.'" placeholder="'.$this->placeHolder.'"';
+        if(!is_null($this->minValue)){
+            $html .= ' min="'.$this->minValue.'"';
+        }
+        if(!is_null($this->maxValue)){
+            $html .= ' max="'.$this->maxValue.'"';
+        }
+        if(!is_null($this->step)){
+            $html .= ' step="'.str_replace(',','.', $this->step).'"';
+        }
+        $html .= '>'; //Close the input tag
+        $html .= '</div>';
+        return [
+            'name' => $this->inputName,
+            'html' => $html
+        ];
     }
 }
