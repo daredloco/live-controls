b5a5dded5e3518da278fa5501573eb462b234a54
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-29 11:10:53 -0300

    Fixed wrong type of $inputType

--- src/Objects/SweetAlert/Inputs/NumericInput.php
+++ src/Objects/SweetAlert/Inputs/NumericInput.php
@@ -3,8 +3,8 @@
 namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 
 class NumericInput extends Input {
-    public $inputType = 'number';
-    
+    public string $inputType = 'number';
+
     public function __construct($inputName, $label = "")
     {
         parent::__construct($inputName, $label);
