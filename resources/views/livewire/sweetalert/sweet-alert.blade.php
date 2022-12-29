280a9e141d70e7dc9a84580b615f59956e5de949
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-29 13:51:21 -0300

    Update sweet-alert.blade.php

--- resources/views/livewire/sweetalert/sweet-alert.blade.php
+++ resources/views/livewire/sweetalert/sweet-alert.blade.php
@@ -12,7 +12,7 @@
         if(popupArr["inputFields"] !== false){
             //WITH INPUT
             alert('Inputs are not included in this version!');
-            const sweetAlertValues = Swal.fire({
+            Swal.fire({
                 title: popupArr["title"],
                 text: popupArr["message"],
                 html: popupArr["html"],
