1a1c213c0fb8ec38269509eb1b275ebe2dad0c77
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 15:00:56 -0300

    Added disabled and getName() to main input class

--- src/Objects/SweetAlert/Inputs/Input.php
+++ src/Objects/SweetAlert/Inputs/Input.php
@@ -12,13 +12,15 @@ class Input{
 
     public string $class = 'form-control';
     public string $inputType = 'text';
+    public bool $disabled = false;
 
-    public function __construct($inputName, $value = "", string $label = null, $placeHolder = "", $parentClass ="mt-3"){
+    public function __construct($inputName, $value = "", string $label = null, $placeHolder = "", bool $disabled = false, $parentClass ="mt-3"){
         $this->inputName = $inputName;
         $this->label = $label;
         $this->placeHolder = $placeHolder;
         $this->parentClass = $parentClass;
         $this->value = $value;
+        $this->disabled = $disabled;
     }
 
     public function toArray():array{
@@ -26,11 +28,15 @@ class Input{
         if($this->label != null && $this->label != ''){
             $html.= '<label for="'.$this->inputName.'" class="form-label">'.$this->label.'</label>';
         }
-        $html .= '<input type="'.$this->inputType.'" class="'.$this->class.'" name="'.$this->inputName.'" id="'.$this->inputName.'" value="'.$this->value.'" placeholder="'.$this->placeHolder.'">';
+        $html .= '<input type="'.$this->inputType.'" class="'.$this->class.'" name="'.$this->inputName.'" id="'.$this->inputName.'" value="'.$this->value.'" placeholder="'.$this->placeHolder.'"'.($this->disabled ? ' disabled' : '').'>';
         $html .= '</div>';
         return [
             'name' => $this->inputName,
             'html' => $html
         ];
+    }
+
+    public function getName():string{
+        return $this->inputName;
     }
 }
