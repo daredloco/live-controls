7e0e15511d4d298c81bf4496522a2b18f5fde3d1
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 15:25:11 -0300

    Added min/max to DateInput

--- src/Objects/SweetAlert/Inputs/DateInput.php
+++ src/Objects/SweetAlert/Inputs/DateInput.php
@@ -2,10 +2,10 @@
 
 namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 
-class DateInput extends Input {
-    public function __construct($inputName, $value = "", string $label = "")
+class DateInput extends NumericInput {
+    public function __construct($inputName, $value = "", string $label = "", string $placeHolder, string $min, string $max)
     {
-        parent::__construct($inputName, $value, $label);
+        parent::__construct($inputName, $value, $label, $placeHolder, $min, $max);
         $this->inputType = "date";
     }
 }
