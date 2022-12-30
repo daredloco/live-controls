15aa85390379590e30e67b1f2e87e4e3e23808cb
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 15:15:14 -0300

    Fixed exception in InputGroup

--- src/Objects/SweetAlert/Inputs/InputGroup.php
+++ src/Objects/SweetAlert/Inputs/InputGroup.php
@@ -14,6 +14,7 @@ class InputGroup
     {
         $this->groupId = $groupId;
         $this->groupClass = $class;
+        $this->inputs = [];
     }
 
     public function add(TextInput|NumericInput|ColorInput|DateInput|TimeInput ...$inputs){
