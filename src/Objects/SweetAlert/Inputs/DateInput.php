5f03a808415e9b76b82fe399d70a36eef5718f4a
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 15:28:42 -0300

    Removed placeholder from DateInput

--- src/Objects/SweetAlert/Inputs/DateInput.php
+++ src/Objects/SweetAlert/Inputs/DateInput.php
@@ -3,9 +3,9 @@
 namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 
 class DateInput extends NumericInput {
-    public function __construct($inputName, $value = "", string $label = "", string $placeHolder, string $min, string $max)
+    public function __construct($inputName, $value = "", string $label = "", string $min, string $max)
     {
-        parent::__construct($inputName, $value, $label, $placeHolder, $min, $max);
+        parent::__construct($inputName, $value, $label, "", $min, $max);
         $this->inputType = "date";
     }
 }
