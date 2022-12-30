a442950608a05aa21a84b3a9bc6a9f537dda91be
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 16:36:10 -0300

    Added size to SelectInput

--- src/Objects/SweetAlert/Inputs/SelectInput.php
+++ src/Objects/SweetAlert/Inputs/SelectInput.php
@@ -5,9 +5,11 @@ namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 class SelectInput extends Input
 {
     public array $options;
+    public ?int $size;
 
-    public function __construct(string $inputName, array $options, string|int $value, ?string $label = null, $parentClass = "mt-3")
+    public function __construct(string $inputName, array $options, string|int $value, ?string $label = null, ?int $size = null, $parentClass = "mt-3")
     {
+        $this->size = $size;
         $this->options = $options;
         parent::__construct($inputName, $value, $label, "", $parentClass);
     }
@@ -17,7 +19,7 @@ class SelectInput extends Input
         if($this->label != null && $this->label != ''){
             $html.= '<label for="'.$this->inputName.'" class="form-label">'.$this->label.'</label>';
         }
-        $html .= '<select id="'.$this->inputName.'" name="'.$this->inputName.'" class="form-select">';
+        $html .= '<select id="'.$this->inputName.'" name="'.$this->inputName.'" class="form-select" '.(!is_null($this->size) ? 'size="'.$this->size.'"' : '').'>';
         foreach($this->options as $key => $option){
             $html .= '<option value="'.$key.'" '.($this->value == $key ? 'selected' : '').'>'.$option.'</option>';
         }
