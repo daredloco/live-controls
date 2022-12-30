6a13661cf31aed6eed19400f71a581554bdb1ffb
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-30 15:04:26 -0300

    Fixed ColorInput classname

--- src/Objects/SweetAlert/Inputs/ColorInput.php
+++ src/Objects/SweetAlert/Inputs/ColorInput.php
@@ -2,7 +2,7 @@
 
 namespace Helvetiapps\LiveControls\Objects\SweetAlert\Inputs;
 
-class DateInput extends Input {
+class ColorInput extends Input {
     public function __construct($inputName, $value = "", string $label = "")
     {
         parent::__construct($inputName, $value, $label);
