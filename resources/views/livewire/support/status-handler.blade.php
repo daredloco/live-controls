40d4789fcc1cebcade418df2063ab03e9eb3d452
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-31 16:22:19 -0300

    Fixed localization naming

--- resources/views/livewire/support/status-handler.blade.php
+++ resources/views/livewire/support/status-handler.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="mb-3">
-        <label for="" class="form-label">{{ __('livecontrols.support.status') }}</label>
+        <label for="" class="form-label">{{ __('livecontrols::support.status') }}</label>
         <select class="form-select" name="" id="" wire:model='newStatus'>
             <option value="0">{{ __('livecontrols::support.status_0') }}</option>
             <option value="1">{{ __('livecontrols::support.status_1') }}</option>
             <option value="2">{{ __('livecontrols::support.status_2') }}</option>
         </select>
     </div>
-    <button class="btn btn-primary" wire:click='updateStatus'>{{ __('livecontrols::status.update_status') }}</button>
+    <button class="btn btn-primary" wire:click='updateStatus'>{{ __('livecontrols::support.update_status') }}</button>
 </div>
