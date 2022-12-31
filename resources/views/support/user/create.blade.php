b8384204d7f8afcd9ce9089270fbfd72ff662a67
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-31 10:03:14 -0300

    Added loc to views.support.user.create

--- resources/views/support/user/create.blade.php
+++ resources/views/support/user/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="h4 font-weight-bold">
-            {{ __('New Support Ticket') }}
+            {{ __('livecontrols::support.new_ticket') }}
         </h2>
     </x-slot>
 
@@ -16,32 +16,32 @@
                             @csrf
 
                             <div class="mb-3">
-                                <x-jet-label value="{{ __('Title') }}" /><br>
+                                <x-jet-label value="{{ __('livecontrols::general.title') }}" /><br>
                                 <x-jet-input class="{{ $errors->has('title') ? 'is-invalid' : '' }}" type="text" name="title"
                                              :value="old('title')" required autofocus />
                                 <x-jet-input-error for="title"></x-jet-input-error>
                             </div>
 
                             <div class="mb-3">
-                              <label for="body" class="form-label">Describe your Problem</label>
+                              <label for="body" class="form-label">{{ __('livecontrols::support.describe_problem') }}</label>
                               <textarea class="form-control" name="body" id="body" rows="3">{{ old('body') }}</textarea>
                             </div>
                             
                             <div class="mb-3">
-                                <label for="priority" class="form-label">Priority</label>
+                                <label for="priority" class="form-label">{{ __('livecontrols::support.priority') }}</label>
                                 <select class="form-select" name="priority" id="priority">
-                                    <option selected>Select one</option>
-                                    <option value="1" @if(old('priority') == 1) selected @endif>Low</option>
-                                    <option value="2" @if(old('priority') == 2) selected @endif>Medium</option>
-                                    <option value="3" @if(old('priority') == 3) selected @endif>High</option>
-                                    <option value="4" @if(old('priority') == 4) selected @endif>Critical</option>
+                                    <option selected>{{ __('Select one') }}</option>
+                                    <option value="1" @if(old('priority') == 1) selected @endif>{{ __('livecontrols::support.low') }}</option>
+                                    <option value="2" @if(old('priority') == 2) selected @endif>{{ __('livecontrols::support.medium') }}</option>
+                                    <option value="3" @if(old('priority') == 3) selected @endif>{{ __('livecontrols::support.high') }}</option>
+                                    <option value="4" @if(old('priority') == 4) selected @endif>{{ __('livecontrols::support.critical') }}</option>
                                 </select>
                             </div>
 
                             <div class="mb-0">
                                 <div class="d-flex justify-content-end align-items-baseline">
                                     <x-jet-button>
-                                        {{ __('Create') }}
+                                        {{ __('livecontrols::general.create') }}
                                     </x-jet-button>
                                 </div>
                             </div>
