1e32aa96164f5424d2a9dfcd136715cdc0c8045a
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-31 14:43:51 -0300

    Added german localization

---
+++ lang/de/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "dashboard" => "Dashboard",
+    "users" => "Benutzer",
+    "user_group" => "Benutzergruppe",
+    "user_groups" => "Benutzergruppen",
+    "groups" => "Gruppen",
+    "permission" => "Berechtigung",
+    "permissions" => "Berechtigungen",
+    "permission_removed" => "Berechtigung entfernt!",
+    "permission_granted" => "Berechtigung hinzugefügt!",
+    "user_added_to_group" => "Benutzer zur Berechtigung hinzugefügt!",
+    "user_removed_from_group" => "Benutzer von Berechtigung entfernt!"
+];
---
+++ lang/de/general.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    "admin_dashboard" => "Administratoren Dashboard",
+    "create" => "Erstellen",
+    "edit" => "Bearbeiten",
+    "delete" => "Löschen",
+    "update" => "Aktualisieren",
+    "add_type" => ":type hinzufügen",
+    "edit_type" => ":type bearbeiten",
+    "search" => "Suchen...",
+    "title" => "Titel",
+    "send" => "Abschicken",
+    "remove" => "Entfernen",
+    "close" => "Schliessen",
+    "id" => "ID",
+    "name" => "Name",
+    "key" => "Schlüssel",
+    "description" => "Beschreibung",
+    "color" => "Farbe",
+    "email" => "Email",
+    "actions" => "Aktionen",
+    "date_format" => "d.m.Y",
+    "date_time_format" => "d.m.Y H:i:s",
+    "time_format" => "H:i:s",
+    "type_created" => ":type erstellt!",
+    "type_not_created" => "Konnte :type nicht erstellen",
+    "type_updated" => ":type aktualisiert!",
+    "type_not_updated" => "Konnte :type nicht aktualisieren",
+    "type_deleted" => ":type gelöscht!",
+    "type_not_deleted" => "Konnte :type nicht löschen"
+];
