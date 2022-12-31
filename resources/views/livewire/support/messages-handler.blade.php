0c60ad6629e5c7e316ae3f8009ae3accd9e4df04
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-31 09:37:21 -0300

    Added localization for livewire.support.messages-handler

--- resources/views/livewire/support/messages-handler.blade.php
+++ resources/views/livewire/support/messages-handler.blade.php
@@ -1,12 +1,12 @@
 <div>
     <div class="mb-3">
       <input type="text"
-        class="form-control" name="msg_title" id="msg_title" placeholder="Title" wire:model='newTitle'>
+        class="form-control" name="msg_title" id="msg_title" placeholder="{{ __('Title') }}" wire:model='newTitle'>
     </div>
     <div class="mb-3">
-      <textarea class="form-control" name="msg_body" id="msg_body" rows="3" wire:model='newBody' placeholder="Write your message..."></textarea>
+      <textarea class="form-control" name="msg_body" id="msg_body" rows="3" wire:model='newBody' placeholder="{{ __('Write your message...') }}"></textarea>
     </div>
-    <button class="btn btn-success text-white" wire:click='sendMessage'>Send</button>
+    <button class="btn btn-success text-white" wire:click='sendMessage'>{{ __('Send') }}</button>
     <hr>
     <ul class="list-group">
         @foreach($supportMessages as $supportMessage)
@@ -18,10 +18,10 @@
                 {!! nl2br($supportMessage->body) !!}
                 <hr>
                 @if(auth()->id() == $supportMessage->user_id || auth()->user()->support_team)
-                <a href="#" wire:click.prevent='removeMessage({{$supportMessage->id}})' style="color: rgb(184, 4, 4);">Remove</a>
+                <a href="#" wire:click.prevent='removeMessage({{$supportMessage->id}})' style="color: rgb(184, 4, 4);">{{ __('Remove') }}</a>
                 <br>
                 @endif
-                <small class="text-muted">{{ $supportMessage->user->name.' at '.$supportMessage->created_at->format('d.m.Y H:i:s') }}</small>
+                <small class="text-muted">{{ $supportMessage->user->name.' at '.$supportMessage->created_at->format(__('dateTimeFormat')) }}</small>
             </li>
         @endforeach
     </ul>
