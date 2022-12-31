b939da3f1e9b9b5d91f5e86cac3d5123abc70c18
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-31 09:31:01 -0300

    Added localization for livewire.admin.permission-list

--- resources/views/livewire/admin/permission-list.blade.php
+++ resources/views/livewire/admin/permission-list.blade.php
@@ -1,17 +1,17 @@
 <div>
     <div class="col-md-4">
-        <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." wire:model='search'>
+        <input type="text" class="form-control" placeholder="{{ __('Search...') }}" aria-label="{{ __('Search...') }}" wire:model='search'>
     </div>
 
     <div class="table-responsive">
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Key</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Actions</th>
+                    <th scope="col">{{ __('ID') }}</th>
+                    <th scope="col">{{ __('Name') }}</th>
+                    <th scope="col">{{ __('Key') }}</th>
+                    <th scope="col">{{ __('Description') }}</th>
+                    <th scope="col">{{ __('Actions') }}</th>
                 </tr>
             </thead>
             <tbody>
@@ -22,8 +22,8 @@
                         <td>{{ $permission->key }}</td>
                         <td>{{ $permission->description }}</td>
                         <td>
-                            <a href="{{ route('livecontrols.admin.userpermissions.edit', ['userPermission' => $permission->id]) }}">Edit</a> 
-                            <a href="{{ route('livecontrols.admin.userpermissions.delete', ['userPermission' => $permission->id]) }}" onclick="event.preventDefault(); document.delete{{ $permission->id }}Form.submit();">Delete</a>
+                            <a href="{{ route('livecontrols.admin.userpermissions.edit', ['userPermission' => $permission->id]) }}">{{ __('Edit') }}</a> 
+                            <a href="{{ route('livecontrols.admin.userpermissions.delete', ['userPermission' => $permission->id]) }}" onclick="event.preventDefault(); document.delete{{ $permission->id }}Form.submit();">{{ __('Delete') }}</a>
                             <form name="delete{{$permission->id}}Form" action="{{ route('livecontrols.admin.userpermissions.delete', ['userPermission' => $permission->id]) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
@@ -34,6 +34,6 @@
             </tbody>
         </table>
     </div>
-    <a href="{{ route('livecontrols.admin.userpermissions.create') }}" class="btn btn-success text-white">Create</a>
+    <a href="{{ route('livecontrols.admin.userpermissions.create') }}" class="btn btn-success text-white">{{ __('Create') }}</a>
 
 </div>
