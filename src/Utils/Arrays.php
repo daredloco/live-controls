dd6150aabef8ae2fd158c8100fe2039522bb9e56
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-01-02 11:34:02 -0300

    Update Arrays.php

--- src/Utils/Arrays.php
+++ src/Utils/Arrays.php
@@ -4,6 +4,14 @@ namespace Helvetiapps\LiveControls\Utils;
 
 class Arrays
 {
+    /**
+     * Returns the value of $array[$key] or the $default value
+     *
+     * @param [type] $key
+     * @param array $array
+     * @param [type] $default
+     * @return mixed
+     */
     public static function array_get($key, array $array, $default = null) : mixed{
         if(array_key_exists($key, $array)){
             return $array[$key];
@@ -11,6 +19,13 @@ class Arrays
         return $default;
     }
 
+    /**
+     * Removed $array[$key] and overwrites the $array with the new array
+     *
+     * @param [type] $key
+     * @param array $array
+     * @return void
+     */
     public static function array_remove($key, array &$array){
         $newArray = [];
         foreach($array as $oldKey => $oldValue){
@@ -21,6 +36,13 @@ class Arrays
         $array = $newArray;
     }
 
+    /**
+     * Removes the value $value inside $array and overwrites $array with the new array
+     *
+     * @param [type] $value
+     * @param array $array
+     * @return void
+     */
     public static function array_remove_value($value, array &$array){
         $newArray = [];
         foreach($array as $oldValue){
