29eca30e49a2d739d7c0d682a10f72f264819d39
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-01-07 16:12:56 -0300

    Added subscriptions_value attribute and fixed namespace

--- src/Traits/Subscriptions/HasSubscriptions.php
+++ src/Traits/Subscriptions/HasSubscriptions.php
@@ -1,14 +1,23 @@
 <?php
 
-namespace Helvetiapps\LiveControls\Traits\Support;
+namespace Helvetiapps\LiveControls\Traits\Subscriptions;
 
 use Helvetiapps\LiveControls\Models\Support\SupportTicket;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-trait HasSupport{
+trait HasSubscriptions{
     public function subscriptions():BelongsToMany
     {
         return $this->belongsToMany(User::class, 'livecontrols_user_subscriptions', 'user_id', 'subscription_id')->withPivot(['due_date', 'value_in_cents']);
+    }
+
+    public function getSubscriptionsValueAttribute():int
+    {
+        $totalValue = 0;
+        foreach($this->subscriptions as $subscription){
+            $totalValue += $subscription->pivot->value_in_cents;
+        }
+        return $totalValue;
     }
 }
