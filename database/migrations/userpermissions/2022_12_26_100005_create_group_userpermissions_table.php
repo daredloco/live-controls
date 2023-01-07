7936c57b5a308cc6a89d4f60541e3bc9e5a14d9f
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-01-07 10:35:12 -0300

    Fixed wrong naming in migration down()

--- database/migrations/userpermissions/2022_12_26_100005_create_group_userpermissions_table.php
+++ database/migrations/userpermissions/2022_12_26_100005_create_group_userpermissions_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_userpermissions');
+        Schema::dropIfExists('livecontrols_group_userpermissions');
     }
 };
