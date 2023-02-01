5c84a7f774b6e72e31e8fc590fdd6c0f5b82b39b
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-02-01 20:34:55 -0300

    Added eventTimeFormat

--- resources/views/livewire/calendar/calendar.blade.php
+++ resources/views/livewire/calendar/calendar.blade.php
@@ -5,7 +5,13 @@
           var calendarEl = document.getElementById('{{ $elementId }}');
           var calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
-            events: @json($convertedEvents)
+            events: @json($convertedEvents),
+            eventTimeFormat: {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              meridiem: false
+            }
           });
           calendar.setOption('locale', '{{ $locale }}');
           calendar.render();
