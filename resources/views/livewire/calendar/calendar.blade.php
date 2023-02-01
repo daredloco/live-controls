c815a1d65a962b713bb73a9a5f91479c6ce37984
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-02-01 20:37:20 -0300

    Update calendar.blade.php

--- resources/views/livewire/calendar/calendar.blade.php
+++ resources/views/livewire/calendar/calendar.blade.php
@@ -9,7 +9,6 @@
             eventTimeFormat: {
               hour: '2-digit',
               minute: '2-digit',
-              second: '2-digit',
               meridiem: false
             }
           });
