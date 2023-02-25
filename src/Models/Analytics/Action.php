7968836ca58c88de1ad1b049f3672105f832c994
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-02-25 17:54:56 -0300

    Added analytics models and changed request model

---
+++ src/Models/Analytics/Action.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Helvetiapps\LiveControls\Models\Analytics;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Action extends Model{
+    use HasFactory;
+
+    protected $table = 'livecontrols_analytics_actions';
+
+    protected $fillable = [
+        'name',
+        'key',
+        'description',
+        'active'
+    ];
+
+    protected $casts = [
+        'active' => 'boolean'
+    ];
+
+    public function requests(): BelongsToMany
+    {
+        return $this->belongsToMany(Request::class, 'livecontrols_analytics_action_requests', 'analytics_request_id', 'analytics_action_id');
+    }
+}
---
+++ src/Models/Analytics/Campaign.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Helvetiapps\LiveControls\Models\Analytics;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Campaign extends Model{
+    use HasFactory;
+
+    protected $table = 'livecontrols_analytics_campaigns';
+
+    protected $fillable = [
+        'name',
+        'key',
+        'description',
+        'active'
+    ];
+
+    protected $casts = [
+        'active' => 'boolean'
+    ];
+
+    public function requests(): BelongsToMany
+    {
+        return $this->belongsToMany(Request::class, 'livecontrols_analytics_campaign_requests', 'analytics_request_id', 'analytics_campaign_id');
+    }
+}
