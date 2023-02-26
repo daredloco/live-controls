afb8dffc8aeed8012b1e949fe976ebe45fd7e0b0
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-02-26 06:48:52 -0300

    Added data lists to analyticsadmin

--- src/Http/Livewire/Admin/AnalyticsAdmin.php
+++ src/Http/Livewire/Admin/AnalyticsAdmin.php
@@ -2,11 +2,18 @@
 
 namespace Helvetiapps\LiveControls\Http\Livewire\Admin;
 
+use Helvetiapps\LiveControls\Models\Analytics\Action;
+use Helvetiapps\LiveControls\Models\Analytics\Campaign;
+use Helvetiapps\LiveControls\Models\Analytics\Request;
 use Livewire\Component;
 
 class AnalyticsAdmin extends Component
 {
     public $page;
+
+    public $userRequests;
+    public $campaigns;
+    public $actions;
 
     protected $queryString = [
         'page' => ['as' => 'p']
@@ -21,6 +28,9 @@ class AnalyticsAdmin extends Component
 
     public function render()
     {
+        $this->userRequests = Request::orderBy('created_at', 'desc')->paginate();
+        $this->campaigns = Campaign::where('active', '=', true)->orderBy('name')->paginate();
+        $this->actions = Action::where('active', '=', true)->orderBy('name')->paginate();
         return view('livecontrols::livewire.admin.analytics-admin');
     }
 }
