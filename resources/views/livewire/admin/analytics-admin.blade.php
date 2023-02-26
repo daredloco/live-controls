bf4f8dc7d9bcc9156231e4a47735e7548e65d5e3
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-02-26 08:21:55 -0300

    Moved date & time to front of table

--- resources/views/livewire/admin/analytics-admin.blade.php
+++ resources/views/livewire/admin/analytics-admin.blade.php
@@ -31,25 +31,25 @@
         <table class="table table-hover">
           <thead>
             <tr>
+              <th scope="col">Date & Time</th>
               <th scope="col">Identifier</th>
               <th scope="col">Target Path</th>
               <th scope="col">Preferred Language</th>
               <th scope="col">Languages</th>
               <th scope="col">User Agent</th>
               <th scope="col">Country</th>
-              <th scope="col">Date & Time</th>
             </tr>
           </thead>
           <tbody>
             @foreach($userRequests as $userRequest)
             <tr class="">
-              <td scope="row">{{ $userRequest->identifier }}</td>
+              <th scope="row">{{ $userRequest->created_at }}</th>
+              <td>{{ $userRequest->identifier }}</td>
               <td>{{ $userRequest->target_path }}</td>
               <td>{{ $userRequest->preferred_language }}</td>
               <td>{{ \Helvetiapps\LiveControls\Utils\Utils::array2String($userRequest->languages) }}</td>
               <td>{{ $userRequest->user_agent }}</td>
               <td>{{ $userRequest->country }}</td>
-              <td>{{ $userRequest->created_at }}</td>
             </tr>
             @endforeach
           </tbody>
