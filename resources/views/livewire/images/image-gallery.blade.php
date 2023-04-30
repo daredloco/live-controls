dde2d3ebfb2f261d975c501e05bd590bbba3f96f
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-04-30 17:36:31 -0300

    Debug Image Gallery created

--- resources/views/livewire/images/image-gallery.blade.php
+++ resources/views/livewire/images/image-gallery.blade.php
@@ -1,22 +1,31 @@
 <div>
     Selected Image: {{ $selectedItem }}
-    @foreach($items as $modelsContent)
-    <div class="card shadow-lg mt-3 col-md-3">
-        <div class="card-body">
-            <h4>{{ $modelsContent["name"] }}</h4>
-            @foreach($modelsContent["items"] as $item)
-            <hr>
-                ID: {{ $item["id"] }}<br>
-                Title: {{ $item["title"] }}<br>
-                @foreach($item["columns"] as $columnName => $columnContent)
-                <img src="{{ $columnContent["url"] }}" class="img-fluid"><br>
-                <small class="text-muted">{{ $columnName }}</small>
+        @foreach($items as $modelsContent)
+        <div class="card shadow-lg mt-3">
+            <div class="card-body">
+                <h4>{{ $modelsContent["name"] }}</h4>
                 <div class="row">
-                    <button class="btn btn-primary text-white" wire:click='select("{{ $columnContent["path"]}}")'>Select</button>
+                    @foreach($modelsContent["items"] as $item)
+                    <div class="card col-md-3">
+                        <div class="card-body">
+                            ID: {{ $item["id"] }}<br>
+                            Title: {{ $item["title"] }}<br>
+                            @foreach($item["columns"] as $columnName => $columnContent)
+                                @if(!is_null($columnContent["url"]))
+                                    <img src="{{ $columnContent["url"] }}" class="img-fluid"><br>
+                                    <small class="text-muted">{{ $columnName }}</small>
+                                    <div class="row">
+                                        <button class="btn btn-primary text-white" wire:click='select("{{ $columnContent["path"]}}")'>Select</button>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-            @endforeach
+
+            </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
