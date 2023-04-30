39f6a70a4d1583608fea0d688ac9748c1c940a91
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-04-30 10:00:35 -0300

    Fixed HasImages.uploadImage if replace is true

--- src/Traits/Images/HasImages.php
+++ src/Traits/Images/HasImages.php
@@ -23,7 +23,7 @@ trait HasImages
         if(!$replace && !is_null($this->{$column})){
             throw new Exception('Image in column "'.$column.'" in table "'.$this->getTable().'" already exists and replace is disabled!');
         }elseif($replace && !is_null($this->{$column})){
-            $this->removeImage($this->{$column}, $isPrivate);
+            $this->removeImage($column, $isPrivate);
         }
 
         $table = $this->getTable();
